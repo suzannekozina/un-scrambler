6fac0b036e96635550f7b8cebca0003d4c69a5d6
suzannekozina <kjartan.kozina13@gmail.com>
2016-05-31 08:48:54 -0500

    clearer comments

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 # You can play with this code to get more practice working with strings in Ruby
 # Try running the code
-  # If you are on Windows and do not yet have a dev environment I highly
-  # recommend Atom editor plus script package if you want to experiement with
-  # running Ruby code locally on your computer
-# Try different modifying the code and seeing what you can come up with!
-# This code is free for reuse, but kindly include the license. Thanks!
+# Try the chanllenge starting on line__
+# Try modifying the exisiting code to see what you can come up with!
+
+# If you are on Windows and don't yet have a dev environment
+# and want to experiement with running Ruby code locally on your computer
+## I highly recommend:
+#### Ruby Installer for Windows - http://rubyinstaller.org/
+#### and Atom editor plus script package - https://atom.io/packages/script
+
+# The following code is free for reuse in your own freely distributed project,
+# but kindly include the license. Thanks!
+
 ##################
 # THE PREMISE
 # You want to know what this inspiring quote says, but it's scrambled!
