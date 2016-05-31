48babc9f2a0842e26d1471721529dd0079be3666
suzannekozina <kjartan.kozina13@gmail.com>
2016-05-31 09:04:47 -0500

    first method

--- app.rb
+++ app.rb
@@ -17,6 +17,20 @@
 # You want to know what this inspiring quote says, but it's scrambled!
 # Run the code below and watch the words un-scramble before your eyes.
 
-quote = "The world changes not by being safe but by challenging expectations."
+quote1 = "I wanted a scripting language that was more powerful than Perl, and /
+          more object-oriented than Python. That's why I decided to design my /
+          own language."
 
-sage_and_venue = "Tony Fadell on Udacity Talks"
+sage_and_venue1 = "Matz in interview with Linux Dev Center" + "Link here: " +
+                  "http://www.linuxdevcenter.com/pub/a/linux/2001/11/29/ruby.html"
+
+quote2 = "Use Ruby, and you'll write better code, be more productive, and /
+          enjoy programming more."
+
+sage_and_venue2 = "Dave Thomas, book 'Programming Ruby: The Pragmatic Programmer's Guide'"
+
+quote3 = "The world changes not by being safe but by challenging expectations."
+quote3 = quote3.insert(3, 'X')
+sage_and_venue3 = "Tony Fadell on Udacity Talks"
+
+print quote3
