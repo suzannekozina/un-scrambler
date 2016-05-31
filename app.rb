4403acedae083fb84e1a1581752ebf2e0de5be4e
suzannekozina <kjartan.kozina13@gmail.com>
2016-05-31 08:27:58 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+# You can play with this code to get more practice working with strings in Ruby
+# Try running the code
+  # If you are on Windows and do not yet have a dev environment I highly
+  # recommend Atom editor plus script package if you want to experiement with
+  # running Ruby code locally on your computer
+# Try different modifying the code and seeing what you can come up with!
+# This code is free for reuse, but kindly include the license. Thanks!
+##################
+# THE PREMISE
+# You want to know what this inspiring quote says, but it's scrambled!
+# Run the code below and watch the words un-scramble before your eyes.
+
+quote = "The world changes not by being safe but by challenging expectations."
+
+sage_and_venue = "Tony Fadell on Udacity Talks"
