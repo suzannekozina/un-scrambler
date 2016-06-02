7c2f6ec3d63154a0142181664207509f50a6e91b
suzannekozina <kjartan.kozina13@gmail.com>
2016-06-02 11:33:27 -0500

    first quote un-scrambled

--- app.rb
+++ app.rb
@@ -17,6 +17,48 @@
 # You want to know what this inspiring quote says, but it's scrambled!
 # Run the code below and watch the words un-scramble before your eyes.
 
+quote = ".SNOITATCEPXE GNIGNELAHC YB TUB EFAS GNIEB YB TON SEGNAHC DLROW *Ht"
+puts quote
+sage_and_venue = ".?ony fUOeZZ on YOUci.y .UZ{}ks"
+puts "Sage and venue: #{sage_and_venue}"
+print "*"*10 + "\n"
+
+#***First let's figure out what the quote says
+# Reverse characters in the quote
+quote = quote.reverse
+puts quote
+print "*"*10 + "\n"
+
+quote = quote.swapcase
+puts quote
+print "*"*10 + "\n"
+
+# The third letter still isn't correct. Time to switch it out.
+quote = quote.sub("*","e")
+puts quote
+print "*"*10 + "\n"
+
+#***Now let's figure out who said this and where
+#It looks like we have a lot of escape characters - let's get rid of those
+
+
+# quote = "The world changes not by being safe but by challenging expectations."
+# quote = quote.swapcase
+# quote = quote.sub(/[AEIO]/, '*')
+# quote = quote.squeeze
+# quote = quote.reverse
+# sage_and_venue = "Tony Fadell on Udacity Talks"
+# sage_and_venue = sage_and_venue3.downcase
+# sage_and_venue = sage_and_venue3.gsub('l','Z')
+# sage_and_venue = sage_and_venue3.insert(1,'?')
+# sage_and_venue = sage_and_venue3.insert(-3,'{}')
+# sage_and_venue = sage_and_venue3.tr('uda','YOU')
+# sage_and_venue = sage_and_venue3.tr('t','.')
+# .escape('\*?{}.')
+
+
+
+
 quote1 = "I wanted a scripting language that was more powerful than Perl, and /
           more object-oriented than Python. That's why I decided to design my /
           own language."
@@ -29,19 +71,4 @@ quote2 = "Use Ruby, and you'll write better code, be more productive, and /
 
 sage_and_venue2 = "Dave Thomas, book 'Programming Ruby: The Pragmatic Programmer's Guide'"
 
-quote3 = "The world changes not by being safe but by challenging expectations."
-quote3 = quote3.swapcase
-quote3 = quote3.sub(/[AEIO]/, '*')
-quote3 = quote3.squeeze
-quote3 = quote3.reverse
-sage_and_venue3 = "Tony Fadell on Udacity Talks"
-sage_and_venue3 = sage_and_venue3.downcase
-sage_and_venue3 = sage_and_venue3.gsub('l','Z')
-sage_and_venue3 = sage_and_venue3.insert(1,'?')
-sage_and_venue3 = sage_and_venue3.insert(-3,'{}')
-sage_and_venue3 = sage_and_venue3.tr('uda','YOU')
-sage_and_venue3 = sage_and_venue3.tr('t','.')
 # .escape('\*?{}.')
-puts quote3
-
-print sage_and_venue3
